b675f30fa4b82e7bfb97dfdfd18ed465515267f3
Floriane Coulmance-Gayrard <doau0129@hpcl003.cm.cluster>
2020-04-18 22:43:21 +0200

    some changes

--- mark_adapters.sh
+++ mark_adapters.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-#SBATCH --job-name=01_mark_adapters_ubams
+#SBATCH --job-name=01_mark_adapters
 #SBATCH --partition=carl.p
 #SBATCH --array=1-117
-#SBATCH --output=/user/doau0129/work/chapter1_2/logs/01_mark_adapters_ubams_%A_%a.out
-#SBATCH --error=/user/doau0129/work/chapter1_2/logs/01_mark_adapters_ubams_%A_%a.err
+#SBATCH --output=/user/doau0129/work/chapter1_2/logs/01_mark_adapters_%A_%a.out
+#SBATCH --error=/user/doau0129/work/chapter1_2/logs/01_mark_adapters_%A_%a.err
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=20G
+#SBATCH --mem-per-cpu=30G
 #SBATCH --time=04:00:00
 
-BASE_DIR=/Users/fco/Desktop/BREMEN_OP/chapter1_2/
+BASE_DIR=/user/doau0129/work/chapter1_2/
 
 
 mkdir $BASE_DIR/outputs/01_adapters/
@@ -19,18 +19,18 @@ mkdir $BASE_DIR/outputs/01_adapters/adapters/
 mkdir $BASE_DIR/outputs/01_adapters/metrics/
 mkdir $BASE_DIR/outputs/listoffiles/
 
-ls -1 $BASE_DIR/outputs/00_ubams/ > $BASE_DIR/outputs/listoffiles/ubams.fofn
-
 INPUT_UBAMS=$BASE_DIR/outputs/listoffiles/ubams.fofn
 
-UBAMS=$(cat $INPUT_BAMS | head -n ${SLURM_ARRAY_TASK_ID} | tail -n 1)
+UBAMS=$(cat ${INPUT_UBAMS} | head -n ${SLURM_ARRAY_TASK_ID} | tail -n 1)
+echo $UBAMS
 
-sample=${UBAMS%.*}
+sample1=${UBAMS%.*}
+sample=${sample1%.*}
 echo $sample
 
-# gatk --java-options "-Xmx18G" \
-#   MarkIlluminaAdapters \
-#   -I=$BASE_DIR/outputs/00_ubams/${UBAMS} \
-#   -O=$BASE_DIR/outputs/01_adapters/adapters/${sample}.adapter.bam \
-#   -M=$BASE_DIR/outputs/01_adapters/metrics/${sample}.adapter.metrics.txt \
-#   -TMP_DIR=\$BASE_DIR/temp_files
+gatk --java-options "-Xmx18G" \
+   MarkIlluminaAdapters \
+   -I=$BASE_DIR/outputs/00_ubams/${UBAMS} \
+   -O=$BASE_DIR/outputs/01_adapters/adapters/${sample}.adapter.bam \
+   -M=$BASE_DIR/outputs/01_adapters/metrics/${sample}.adapter.metrics.txt \
+   -TMP_DIR=$BASE_DIR/temp_files
