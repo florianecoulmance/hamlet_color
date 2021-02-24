4df9eeafa1c7b1a6e2eeb4cf5c3892d0c4de8d91
fco <fco@macflo.local>
2021-02-24 15:21:53 +0100

    gxp plots

--- R/gxp_plots.R
+++ R/gxp_plots.R
@@ -1,11 +1,36 @@
+#!/usr/bin/env Rscript
+# by: Floriane Coulmance: 24/02/2021
+# usage:
+# Rscript --vanilla gxp_plots.R <data_path> <figure_path>
+# -------------------------------------------------------------------------------------------------------------------
+# data_path in : $BASE_DIR/outputs/7_gxp/$DATASET
+# figure_path in : $BASE_DIR/outputs/7_gxp/$DATASET/figures/
+# -------------------------------------------------------------------------------------------------------------------
+
+
+# Clear the work space
 rm(list = ls())
 
-
+# Load needed library
 library(ggplot2)
 library(tidyverse)
 library(hypogen)
 library(dplyr)
 library(plyr)
+
+
+# -------------------------------------------------------------------------------------------------------------------
+# ARGUMENTS
+
+# Get the arguments in variables
+args = commandArgs(trailingOnly=FALSE)
+args = args[7:8]
+print(args)
+
+data_path <- as.character(args[1]) # Path to mean coverage data table
+# data_path <- "/Users/fco/Desktop/BREMEN_OP/ibd/coverage_table"
+figure_path <- as.character(args[2]) # Path to the figure folder
+
 
 setwd('/user/doau0129/chapter1_2/outputs/gxp')
 
@@ -13,24 +38,20 @@ setwd('/user/doau0129/chapter1_2/outputs/gxp')
 # df_gxp_bars <- read.table(file = 'bars_body.lm.50k.5k.txt.gz', sep = '\t', header = TRUE)
 # 
 
-files <- list.files(pattern = "k.txt.gz")
-traits <- list("bars_body", "bars_head", "peduncle", "snout", "H.unicolor", "H.aberrans", "H.nigricans", "H.maya", "H.gemma", "H.puella", "H.indigo","H.gummiguta", "Tan.hamlet", "H.guttavarius", "H.floridae", "H.chlorurus")
+files <- list.files(pattern = "lmm.50k.5k.txt.gz")
+traits <- list("PC1", "PC2", "PC3", "PC4", "PC5", "PC6", "PC7", "PC8", "PC9", "PC10")
 
 for(i in 1:length(traits)) {
   
-  pdf(paste0("plot_",traits[i],".pdf",spe=""))
+  pdf(paste0(figure_path,"plot_",traits[i],".pdf",spe=""))
   
   par(mfrow=c(2,2))
   
   for(j in 1:length(files)){
     old_trait <- traitfile
     
-    if(substring(files[j], 1, 1)=="H"){
-      traitfile <- strsplit(files[j], ".lm")[[1]][1]
-      print(traitfile)
-    } else{
-      traitfile <- regmatches(x=files[j],gregexpr('[.]',files[j]),invert=TRUE)[[1]][1]
-      print(traitfile)
+    traitfile <- regmatches(x=files[j],gregexpr('[.]',files[j]),invert=TRUE)[[1]][1]
+    print(traitfile)
       
     }
     
