b796f7f499ce70a25ff80c1922fc3ea9b557376c
fco <fco@macflo.local>
2022-01-27 19:46:47 +0100

    fst

--- R/fst_plots.R
+++ R/fst_plots.R
@@ -1,9 +1,10 @@
 # by: Floriane Coulmance: 01/05/2020
 # usage:
-# Rscript fst_plots.R <fst_directory_path> <global_fst_file_path>
+# Rscript fst_plots.R <fst_directory_path> <global_fst_file_path> <figure_path>
 # -------------------------------------------------------------------------------------------------------------------
 # fst_directory_path : $BASE_DIR/outputs/8_fst/
 # global_fst_file_path : $BASE_DIR/outputs/8_fst/fst_globals.txt
+# figure_path : $BASE_DIR/figures/fst/
 # -------------------------------------------------------------------------------------------------------------------
 
 
@@ -20,6 +21,7 @@ library(dplyr)
 library(plyr)
 library(ggpubr)
 library(vroom)
+
 
 # -------------------------------------------------------------------------------------------------------------------
 # ARGUMENTS
@@ -52,23 +54,17 @@ pairwise_table <- function(path,file_list) {
 
   # Takes a list of files and combine their content into 1 table
 
-  run_files <- file_list %>% str_extract(., "[^.]+") %>%
-             #str_sub(.,1,11) %>%
-             str_replace(.,pattern = '([a-z]{3})-([a-z]{3})-([a-z]{3})', '\\2\\1-\\3\\1')
+  # Create list of dataset names
+  run_files <- file_list %>%
+               str_extract(., "[^.]+") %>% # extract string before first occurence of "." in file name
+               str_replace(.,pattern = '([a-z]{3})-([a-z]{3})-([a-z]{3})', '\\2\\1-\\3\\1') # replace _ by -
 
-  print(run_files)
-
+  # Combine data from all dataset and keep track of dataset for each row
   data <- purrr::pmap(tibble(file = str_c(path,file_list),run = run_files),hypo_import_windows) %>%
           bind_rows() %>%
           set_names(., nm = c('CHROM', 'BIN_START', 'BIN_END', 'N_VARIANTS', 'WEIGHTED_FST', 'MEAN_FST', 'GSTART', 'POS', 'GPOS', 'run')) %>%
           separate(run, into =c("loc","pop1","pop2")) %>% 
           mutate(run = str_c(pop1,loc,'-',pop2,loc),run = fct_reorder(run,WEIGHTED_FST))
-  
-  #print(data$run)
-  #print(distinct(data$run))
-  #print(n_distinct(data$run))
-  print(unique(data[["run"]]))
-  print(length(unique(data[["run"]])))
 
   return(data)
 
@@ -80,16 +76,30 @@ fst_global_table <- function(file) {
   # Format global fst table with run columns, pair of species and location 
   
   global1 <- file %>% 
-    mutate(names = gsub("^.{0,44}", "", names)) %>% 
-    separate(names, c("dataset","pair1", "pair2"), sep = "_", extra = "merge") %>%
-    mutate(run = str_c(pair1,dataset,'-',pair2,dataset),
-           run = fct_reorder(run,weighted_fst)) %>%
-    relocate(dataset, run, pair1, pair2, weighted_fst, mean_fst) %>%
-    filter(weighted_fst != "NaN" & mean_fst != "NaN")
+             mutate(names = gsub("^.{0,44}", "", names)) %>% 
+             separate(names, c("dataset","pair1", "pair2"), sep = "_", extra = "merge") %>%
+             mutate(run = str_c(pair1,dataset,'-',pair2,dataset),
+             run = fct_reorder(run,weighted_fst)) %>%
+             relocate(dataset, run, pair1, pair2, weighted_fst, mean_fst) %>%
+             filter(weighted_fst != "NaN" & mean_fst != "NaN")
   
-  print(global1$run)
   return(global1)
   
+}
+
+
+subset_global <- function(table, param) {
+
+  # Takes a global fst table and subset according to datasets wanted
+  
+  global1 <- if(param=="loc") {
+                table %>% filter(dataset != "nig" & dataset != "pue")
+             } else {
+                table %>% filter(dataset != "bel" & dataset != "boc" & dataset != "puer")
+             }
+
+  return(global1)
+
 }
 
 
@@ -103,29 +113,32 @@ anno_pair_spe <- function (spe, left, right,
   
   # Modified function from GenomicOriginsScripts to get 2 flags around 1 species
 
-  nr_left <- which(hypo_flag$geo %>% str_sub(.,1,3) == left)
+  nr_left <- which(hypo_flag$geo %>% str_sub(.,1,3) == left) # select logo to be at the left of the plot in database
   
-  nr_right <- which(hypo_flag$geo %>% str_sub(.,1,3) == right)
+  nr_right <- which(hypo_flag$geo %>% str_sub(.,1,3) == right) # select logo to be at the right of the plot in database
   
-  nr_spe <- which((hypo_img$spec %>% str_sub(.,1,3)) == spe)
+  nr_spe <- which((hypo_img$spec %>% str_sub(.,1,3)) == spe) # select the logo of the species to be in the middle, from database
   
+  # Combine each components into one big logo plot
   p <- ggplot() +
-    theme_void() +
-    scale_x_continuous(expand = c(0, 0)) +
-    scale_y_continuous(limits = c(-0.4, 0.38)) +
-    annotation_custom(hypo_flag$flag[[nr_right]], xmin = 0.5, xmax = 1.02, ymin = -Inf, ymax = Inf) +
-    annotation_custom(hypo_flag$flag[[nr_left]], xmin = -1.02, xmax = -0.5, ymin = -Inf, ymax = Inf)+
-    annotation_custom(hypo_img$l[[nr_spe]], xmin = -0.3, xmax = .3, ymin = -Inf, ymax = Inf) +
-    coord_cartesian(xlim = c(-1.1,1.1))
+       theme_void() +
+       scale_x_continuous(expand = c(0, 0)) +
+       scale_y_continuous(limits = c(-0.4, 0.38)) +
+       annotation_custom(hypo_flag$flag[[nr_right]], xmin = 0.5, xmax = 1.02, ymin = -Inf, ymax = Inf) +
+       annotation_custom(hypo_flag$flag[[nr_left]], xmin = -1.02, xmax = -0.5, ymin = -Inf, ymax = Inf)+
+       annotation_custom(hypo_img$l[[nr_spe]], xmin = -0.3, xmax = .3, ymin = -Inf, ymax = Inf) +
+       coord_cartesian(xlim = c(-1.1,1.1))
 
   return(p)
 
 }
 
+
 legend_creation <- function(table_global, param) {
   
   # Creates the legend for each panel with logos
   
+  # Create plot for each dataset into and store plots into a list
   grobs <- list(GenomicOriginsScripts::anno_pair_flag(loc = "bel", left = "ind", right = "may") %>%
                   ggplotGrob(),
                 GenomicOriginsScripts::anno_pair_flag(loc = "bel", left = "ind", right = "nig") %>%
@@ -173,12 +186,14 @@ legend_creation <- function(table_global, param) {
                 GenomicOriginsScripts::anno_pair_flag(loc = "pue", left = "pue", right = "uni") %>%
                   ggplotGrob())
   
+  # Put into a table the dataset names and the associated plot
   grob_list <- tibble(dataset = table_global$dataset, run = table_global$run, grob = grobs)
   
+  # Subset according to parameter
   list_logos <- if(param=="loc") {
-                grob_list %>% filter(dataset != "nig" & dataset != "pue")
+                  grob_list %>% filter(dataset != "nig" & dataset != "pue")
                 } else {
-                grob_list %>% filter(dataset != "bel" & dataset != "boc" & dataset != "puer")
+                  grob_list %>% filter(dataset != "bel" & dataset != "boc" & dataset != "puer")
                 }
 
   return(list_logos)
@@ -190,32 +205,40 @@ fst_plots <- function(table_fst, table_global, list_grob, path, prefix) {
   
   # Take fst data table and global fst table to make a faceted plot
   
-  sc_ax <- scales::cbreaks(c(0,max(table_global$weighted_fst)),
-                           scales::pretty_breaks(4))
+  sc_ax <- scales::cbreaks(c(0,max(table_global$weighted_fst)), scales::pretty_breaks(4)) # format x axis preliminary to plot creation
   
+  # Create the plot
   p <- ggplot() +
-    facet_wrap(.~run, as.table = TRUE, ncol = 1,dir = 'v') +
-    geom_vline(data = hypogen::hypo_karyotype,
-               aes(xintercept = GEND),
-               color = hypo_clr_lg) +
-    geom_point(data = table_fst,
-               aes(x = GPOS, y = WEIGHTED_FST),
-               size=.2) +
-    geom_hypo_grob(data = list_grob,
-                   aes(grob = grob, x = .9,y = .7),
-                   angle = 0, height = .5, width = .16) +
-    scale_x_hypo_LG(sec.axis =  sec_axis(~ ./hypo_karyotype$GEND[23],
-                                         breaks = (sc_ax$breaks/max(table_global$weighted_fst)),
-                                         labels = sprintf("%.2f", sc_ax$breaks))) +
-    scale_y_continuous(name = expression(italic('F'[ST])),
-                       limits = c(-.1,1),
-                       breaks = c(0,.5,1)) +
-    theme_hypo() +
-    theme(strip.text = element_blank(),
-          legend.position = 'none',
-          axis.title.x = element_text(),
-          axis.text.x.bottom = element_text(colour = 'darkgray'))
+       facet_wrap(.~run, as.table = TRUE, ncol = 1,dir = 'v') +
+       geom_rect(data = table_global %>%
+                 mutate(xmax = xmax * hypo_karyotype$GEND[23]),
+                 aes(xmin = 0, xmax = xmax,
+                 ymin = -Inf, ymax = Inf),
+                 color = rgb(1,1,1,0),
+                 fill = clr_below) +
+       geom_vline(data = hypogen::hypo_karyotype,
+                  aes(xintercept = GEND),
+                  color = hypo_clr_lg) +
+       geom_point(data = table_fst,
+                  aes(x = GPOS, y = WEIGHTED_FST),
+                  size=.2) +
+       geom_hypo_grob(data = list_grob,
+                      aes(grob = grob, x = .9,y = .7),
+                      angle = 0, height = .5, width = .16) +
+       scale_x_hypo_LG(sec.axis =  sec_axis(~ ./hypo_karyotype$GEND[23],
+                                            breaks = (sc_ax$breaks/max(table_global$weighted_fst)),
+                                            labels = sprintf("%.2f", sc_ax$breaks),
+                                            name = expression(Genomic~position/~Genome~wide~weighted~italic(F[ST])))) +
+       scale_y_continuous(name = expression(italic('F'[ST])),
+                          limits = c(-.1,1),
+                          breaks = c(0,.5,1)) +
+       theme_hypo() +
+       theme(strip.text = element_blank(),
+             legend.position = 'none',
+             axis.title.x = element_text(),
+             axis.text.x.bottom = element_text(colour = 'darkgray'))
   
+  # Save plot into png image
   hypo_save(filename = paste0(path,prefix,"_fst.png"),
             plot = p,
             width = 8,
@@ -232,6 +255,7 @@ fst_plots <- function(table_fst, table_global, list_grob, path, prefix) {
 # -------------------------------------------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------------------------------------------
 
+
 # Locate all the files that will be used in plots
 files <- dir(fst_folder, pattern = '.50k.windowed.weir.fst.gz')
 print(files)
@@ -246,34 +270,40 @@ for(f in files){
   }
 }
 
-print(files)
+# print(files)
 
 # Locate the files either corresponding to a location fst or a species fst
 files_loc <- files[grepl("^bel_|^boc_|^puer_", files)]
-print(files_loc)
+# print(files_loc)
 files_spec <- files[grepl("^nig_|^pue_", files)]
-print(files_spec)
+# print(files_spec)
 
 # Create tables for pairwise fst comparisons
 pairwise_loc <- pairwise_table(fst_folder,files_loc)
-print(pairwise_loc)
+# print(pairwise_loc)
 pairwise_spec <- pairwise_table(fst_folder,files_spec)
-print(pairwise_spec)
+# print(pairwise_spec)
 
 # Open the global fst file
 global <- read_tsv(fst_global,col_names=c("names","weighted_fst","mean_fst"))
-print(global)
+# print(global)
 
 # Format global fst table
 global_table <- fst_global_table(global)
-print(global_table)
+# print(global_table)
+
+# Subset global_table according to the different fst plots we want
+global_loc <- subset_global(global_table, "loc")
+print(global_loc)
+global_spec <- subset_global(global_table, "spec")
+print(global_spec)
 
 # Create a list of logos to use in faceted plot in next step
 logos_loc <- legend_creation(global_table, "loc")
-print(logos_loc)
+# print(logos_loc)
 logos_spec <- legend_creation(global_table, "spec")
-print(logos_spec)
+# print(logos_spec)
 
 # Create fst plots
-fst_plots(pairwise_loc, global_table, logos_loc, path_figures, "loc")
-fst_plots(pairwise_spec, global_table, logos_spec, path_figures, "spec")
+fst_plots(pairwise_loc, global_loc, logos_loc, path_figures, "loc")
+fst_plots(pairwise_spec, global_spec, logos_spec, path_figures, "spec")
