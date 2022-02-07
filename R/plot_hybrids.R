6a83f579e02950415fc5b83de94ad9163385cfc1
fco <fco@macflo.local>
2022-02-07 13:26:27 +0100

    hybrid pipeline

---
+++ R/plot_hybrids.R
@@ -0,0 +1,274 @@
+#!/usr/bin/env Rscript
+# by: Floriane Coulmance: 02/02/2022
+# usage:
+# Rscript plot_hybrids.R <newhybrid_directory> <result_directory>
+# -------------------------------------------------------------------------------------------------------------------
+# newhybrid_directory : $BASE_DIR/outputs/9_newhyb/
+# result_directory : $BASE_DIR/figures/9_newhyb/
+# -------------------------------------------------------------------------------------------------------------------
+
+
+# Clear the work space
+rm(list = ls())
+
+# Load needed library
+library(paletteer)
+library(ggthemes)
+library(prismatic)
+library(patchwork)
+library(ggtext)
+library(hypoimg)
+
+
+# -------------------------------------------------------------------------------------------------------------------
+# ARGUMENTS
+
+# -------------------------------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------------------------
+
+
+# Get the arguments in variables
+args = commandArgs(trailingOnly=FALSE)
+args = args[6:7]
+print(args)
+
+base_dir <- as.character(args[1]) # Path to the general NEWHYBRID result folder
+print(base_dir)
+fig_path <- as.character(args[2]) # Path to figure folder
+print(fig_path)
+
+# -------------------------------------------------------------------------------------------------------------------
+# FUNCTIONS
+
+# -------------------------------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------------------------
+
+
+get_subfolder <- function(path, dir_list) {
+  
+  # Takes a list of directory and find path to each of their subsirectories
+  
+  res_folders <- list() # Create empty list to store path to folders
+  
+  # Loop through list of folder in base directory to retrieve important sub folders
+  for (folder in dir_list) {
+    
+    label <- str_split(folder, fixed("_")) # Split folder name to get each element separated by "_"
+    print(label)
+    print(label[0])
+    print(label[1])
+    print(label[2])
+    pop1 <- label[0] # Get 1st population from the pairwise comparison
+    print(pop1)
+    pop2 <- label[1] # Get 2nd population from the pairwise comparison
+    print(pop2)
+    
+    runname <- paste0(pop1,"_",pop2) # Get the pairwise comparison label
+    print(runname)
+    
+    result_folder <- paste0(path,folder,"/NH.Results/newHyb.",runname,".80SNPs.txt_Results/")
+    print(result_folder)
+    
+  }
+  
+  res_folders[folder] <- result_folder
+  print(res_folders)
+  return(res_folders)
+  
+}
+
+
+getPofZ <- function(res_paths) {
+  
+  # Takes a list of subdirectories, for each of them extract files and make a table of data from these extracted files
+  
+  # Get the pairwise comparison label
+  runname <- res_paths %>% 
+    gsub(".*/","") %>% 
+    str_remove("newHyb.") %>% 
+    str_remove(".80SNPs.txt_Results")
+  print(runname)
+  
+  pops <- c(str_sub(runname,1,6),str_sub(runname,-6,-1)) # Get each population of the pairwise comparison separately
+  print(pops)
+  
+  pofz <- dir(res_paths, pattern = "PofZ.txt") # List the Pofz files for each pairwise comparison of the list 
+  print(pofz)
+  inds <- dir(res_paths, pattern = "_individuals.txt") # List the individual files for each pairwise comparison of the list
+  print(inds) 
+  
+  colN <- c("P1", "P1_bc", "P2", "P2_bc") # Set future column names
+  
+  # Open Pofz files and combine them in big table and add list of individual sample names to column from individuals files 
+  NHres <- vroom::vroom(str_c(res_paths, pofz),
+                        delim = '\t',
+                        skip = 1,
+                        col_names  = c('indNR', 'IndivName', colN[1], colN[3], 'F1', 'F2', colN[2], colN[4])) %>%
+    mutate(IndivName = vroom::vroom(str_c(res_paths, inds), 
+                                    delim = ',',
+                                    col_names =  c('IndivName'))[,1] %>%
+             unname() %>%
+             unlist())
+  
+  # Rename column names for big table
+  bin_tib <- tibble(bin_generic = c(colN, "F1", "F2"),
+                    bin = c(paste0(c(pops[1],pops[1],pops[2],pops[2]), c('_pure','_BC','_pure','_BC')), "F1", "F2"))
+  
+  # Create final data table
+  data <- NHres %>%
+    pivot_longer(names_to = 'bin_generic',
+                 values_to= "post_prob",
+                 cols = c(-indNR, -IndivName)) %>%
+    left_join(bin_tib) %>%
+    mutate(run = runname,
+           loc = str_sub(run,-3,-1),
+           ind_order = str_c(str_sub(IndivName,-6,-1),"_",str_sub(IndivName,1,-7)))
+  
+  return(data)
+  
+}
+
+
+plot_loc <- function(fold_paths = fold_paths, loc){
+  
+  # Takes a list of files and subset according to locations to create a plot for the considered location
+  
+  print(fold_paths[str_sub(fold_paths,-22,-20) == loc])
+  
+  # Create big data table combining all files from different pairwise comparison and keep track of pairwise comparison label 
+  data <- map_dfr(.x = fold_paths[str_sub(fold_paths,-22,-20) == loc],
+                  .f = getPofZ)
+  print(data)
+  
+  # Find samples that are hybrids
+  is_hybr <- data %>%
+    filter(!(grepl(pattern = "_pure", bin))) %>%
+    filter(post_prob > .99) %>%
+    .$ind_order %>%
+    unique()
+  print(is_hybr)
+  
+  # List of species names and their abbreviation
+  sp_names <- c(abe = "aberrans",
+                flo = "floridae",
+                gum = "gummigutta",
+                ind = "indigo",
+                may = "maya",
+                nig = "nigricans",
+                pue = "puella",
+                ran = "randallorum",
+                tab = "tabacarius",
+                tor = "tortugarum",
+                uni = "unicolor",
+                chl = "chlorurus")
+  
+  data <- data %>%
+          mutate(ind_label = ifelse(ind_order %in% is_hybr, str_c("**", ind_order, "**"), ind_order),
+                 run2 = str_c("*H. ", sp_names[str_sub(string = run,1,3)],"* - *H. ", sp_names[str_sub(string = run,8,10)],"*"))
+  print(data)
+  
+  # Create labels
+  data_labs <- data %>%
+               filter(!duplicated(ind_order)) %>%
+               select(ind_order, ind_label)
+  print(data_labs)
+  
+  # Create color palette for hybrid, pure line and backcrosses
+  clr <- paletteer_c("ggthemes::Red-Green-Gold Diverging",3) %>%
+         c(.,clr_lighten(.)) %>%
+         color() %>% 
+         .[c(1,4,2,5,6,3)] %>%
+         set_names(nm = c("P1", "P1_bc","F1", "F2", "P2_bc", "P2"))
+  print(clr)
+  
+  # List of location names and labels
+  loc_names <- c(bel = "Belize",
+                 pue = "Puerto Rico",
+                 boc = "Panama",
+                 flo = "Florida")
+  
+  # Create plot panel for a particular location 
+  data  %>%
+    ggplot(aes(x = ind_order, y = post_prob, fill = bin_generic)) +
+    geom_bar(position = 'stack', stat = "identity") +
+    scale_fill_manual(values = clr) +
+    scale_x_discrete(breaks = data_labs$ind_order, labels = data_labs$ind_label) +
+    labs(y = "Posterior probability", title = loc_names[loc]) +
+    facet_grid(run2~.) +
+    theme_minimal() +
+    theme(legend.position = "bottom",
+          strip.text.x  = element_markdown(),
+          strip.text.y  = element_markdown(),
+          axis.text.x = element_markdown(angle = 90),
+          axis.title.x = element_blank(),
+          axis.title.y = element_text(vjust = 4))
+  
+}
+
+
+theme_hyb <-  function(legend.position = "none",...) {
+  
+  # Define a theme for plot presentation
+  
+  list(scale_y_continuous(breaks = c(0,.5,1)),
+       theme(legend.position = legend.position,
+             legend.background = element_rect(fill = "white",colour = rgb(1,1,1,0)),
+             legend.direction = "horizontal",
+             legend.justification = c(1,1),
+             strip.text.y = element_markdown(angle = 0,hjust = 0), ...))
+  
+}
+
+
+final_plot <- function(plots, path) {
+  
+  # Compose figure from the each location panels and save it as a pdf figure 
+  
+  p <- (plots[[1]] +  guides(fill = guide_legend(title = "Hybrid Class")) + theme_hyb(legend.position = c(1,1))) +
+       (plots[[2]] + theme_hyb()) +
+       (plots[[3]] + theme_hyb()) +
+       plot_layout(ncol = 1, heights = c(10,15,3) %>% label_spacer()) +
+       plot_annotation(tag_levels = 'a')
+  
+  hypo_save(filename = paste0(path,"hybrids.pdf"),
+            plot = p,
+            height = 16,
+            width = 10)
+  
+}
+
+
+# -------------------------------------------------------------------------------------------------------------------
+# ANALYSIS
+
+# -------------------------------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------------------------
+
+
+# sp_labs <- c(abe = expression(italic(H.~aberrans)),
+#              flo = expression(italic(H.~floridae)),
+#              gum = expression(italic(H.~gummigutta)),
+#              ind = expression(italic(H.~indigo)),
+#              may = expression(italic(H.~maya)),
+#              nig = expression(italic(H.~nigricans)),
+#              pue = expression(italic(H.~puella)),
+#              ran = expression(italic(H.~randallorum)),
+#              tab = expression(italic(S.~tabacarius)),
+#              tor = expression(italic(S.~tortugarum)),
+#              uni = expression(italic(H.~unicolor)),
+#              chl = expression(italic(H.~chlorurus)))
+
+sub_dir <- list.dirs(path = base_dir, full.names = F, recursive = F)
+print(sub_dir)
+
+fold_paths <- get_subfolder(base_dir, sub_dir)
+print(fold_paths)
+
+p_loc <- c("boc", "bel", "por") %>% map(plot_loc)
+print(p_loc)
+
+# bocdat <- p_loc[[1]]$data
+# beldat <- p_loc[[2]]$data
+# puedat <- p_loc[[3]]$data
+
+final_plot(p_loc, fig_path)
---
+++ hybrids.sh
@@ -0,0 +1,385 @@
+#!/bin/bash
+# by: Floriane Coulmance: 01/04/2020
+# usage:
+# gxp.sh -i <PATH> -j <JOB_ID>
+# ------------------------------------------------------------------------------
+# PATH corresponds to the path to the base directory, all outputs and necessary
+# folder will be created by the script
+# JOB_ID corresponds string ids from where you want  the script to be ran
+# ------------------------------------------------------------------------------
+
+
+
+# ********** Allow to enter bash options **********
+# -------------------------------------------------
+
+while getopts i:j: option
+do
+case "${option}"
+in
+i) BASE_DIR=${OPTARG};; # get the base directory path
+j) JID_RES=${OPTARG};; # get the jobid from which you want to resume
+esac
+done
+
+
+
+# ********* Create necessary repositories *********
+# -------------------------------------------------
+
+# Repo for job logs
+mkdir $BASE_DIR/logs/
+
+# Repo for figures
+mkdir $BASE_DIR/figures/
+
+# Outputs repo
+mkdir $BASE_DIR/outputs/
+mkdir $BASE_DIR/outputs/8_hybrids/
+
+# Annex folder for list of files
+mkdir $BASE_DIR/outputs/lof/
+
+
+
+# ********* Jobs creation *************************
+# -------------------------------------------------
+
+# --------------------------- PREPARATION -------------------------------------#
+
+# ------------------------------------------------------------------------------
+# Job 0 creates the species-location pairs
+
+jobfile0=0_pairs.tmp # temp file
+cat > $jobfile0 <<EOA # generate the job file
+#!/bin/bash
+#SBATCH --job-name=0_pairs
+#SBATCH --partition=carl.p
+#SBATCH --array=0-11
+#SBATCH --output=$BASE_DIR/logs/0_pairs_%A_%a.out
+#SBATCH --error=$BASE_DIR/logs/0_pairs_%A_%a.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=20G
+#SBATCH --time=04:00:00
+
+
+INPUT=$BASE_DIR/outputs/09_1_snpfiltration/snp_filterd.vcf.gz                                     # input the genotyping file with SNPs created by step 14 of genotyping.sh pipeline
+
+tr=(indbel/maybel indbel/nigbel indbel/puebel maybel/nigbel maybel/puebel nigbel/puebel           # create list of species pairwise comparison keeping populations that have >5 individuals represented
+nigboc/pueboc nigboc/uniboc pueboc/uniboc chlpue/puepue chlpue/unipue puepue/unipue)
+
+PAIR=\${tr[\${SLURM_ARRAY_TASK_ID}]}                                                              # create 1 job (array job) per species pairwise comparison 
+echo \${PAIR}                                               
+POP_UN=\${PAIR%/*}                                                                                # extract both population of the pairwise comparison 
+echo \${POP_UN}                                                                                   # print the pairwise comparison label and both population label
+POP_DEUX=\${PAIR##*/}
+echo \${POP_DEUX}
+
+vcfsamplenames \${INPUT} | grep \${POP_UN} > $BASE_DIR/outputs/8_hybrids/\${POP_UN}.pop           # create list of individuals per population of the pairwise comparison 
+vcfsamplenames \${INPUT} | grep \${POP_DEUX} > $BASE_DIR/outputs/8_hybrids/\${POP_DEUX}.pop       # put the list of individuals in 1 file for both of the populations
+
+vcftools --gzvcf \${INPUT} \
+    --weir-fst-pop $BASE_DIR/outputs/8_hybrids/\${POP_UN}.pop \                                   # calculate pairwise Fst and save in file
+    --weir-fst-pop $BASE_DIR/outputs/8_hybrids/\${POP_DEUX}.pop \
+    --stdout | gzip > $BASE_DIR/outputs/8_hybrids/\${POP_UN}_\${POP_DEUX}.fst.tsv.gz
+
+ls -1 $BASE_DIR/outputs/8_hybrids/*.fst.tsv.gz > $BASE_DIR/outputs/lof/8_fst.fofn                 # add all pairwise Fst file names and path to a file
+
+
+EOA
+
+
+
+# ------------------------------------------------------------------------------
+# Job 1 creates the files with information on the position of the 800 most 
+# differentiated SNPs
+
+jobfile1=1_snp.tmp # temp file
+cat > $jobfile1 <<EOA # generate the job file
+#!/bin/bash
+#SBATCH --job-name=1_snp
+#SBATCH --partition=carl.p
+#SBATCH --array=1-12
+#SBATCH --output=$BASE_DIR/logs/1_snp_%A_%a.out
+#SBATCH --error=$BASE_DIR/logs/1_snp_%A_%a.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=20G
+#SBATCH --time=00:15:00
+
+
+module load hpc-env/8.3                                                                           # load the environment for the latest version of R on the Oldenburg Cluster
+module load R/4.0.2-foss-2019b
+
+INPUT_PAIRS=$BASE_DIR/outputs/lof/8_fst.fofn                                                      # input file with fst file names and path
+PAIR=\$(cat \${INPUT_PAIRS} | head -n \${SLURM_ARRAY_TASK_ID} | tail -n 1)                        # create a job for each filename in the input file and extract the path to the file corresponding to the job array number
+echo \${PAIR}
+
+# PREFIX=\${PAIR##*/} 
+# echo \${PREFIX}
+
+PREFIX2=\${PAIR%%.*}                                                                              # create prefix (with path and future file name without the extension)
+echo \${PREFIX2}
+
+Rscript $BASE_DIR/R/filter_snps.R \${PAIR} 800 \${PREFIX2}                                        # execute R code that will select 800 random positions in Fst file
+
+
+EOA
+
+
+
+# ------------------------------------------------------------------------------
+# Job 2 creates the genotyping files with only the 800 most differentiated SNPs
+
+jobfile2=2_vcf.tmp # temp file
+cat > $jobfile2 <<EOA # generate the job file
+#!/bin/bash
+#SBATCH --job-name=2_vcf
+#SBATCH --partition=carl.p
+#SBATCH --array=1-12
+#SBATCH --output=$BASE_DIR/logs/2_vcf_%A_%a.out
+#SBATCH --error=$BASE_DIR/logs/2_vcf_%A_%a.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=50G
+#SBATCH --time=03:00:00
+
+
+INPUT=$BASE_DIR/outputs/old/09_1_snpfiltration/snp_filterd.vcf.gz                                 # input the genotyping file with SNPs created by step 14 of genotyping.sh pipeline
+echo \${INPUT}
+
+INPUT_PAIRS=$BASE_DIR/outputs/lof/8_fst.fofn                                                      # input the list of pairwise Fst files created at job0
+PAIR=\$(cat \${INPUT_PAIRS} | head -n \${SLURM_ARRAY_TASK_ID} | tail -n 1)                        # create a job per Fst file
+echo \${PAIR}
+
+PREFIX=\${PAIR##*/}                                                                               # retrieve file prefix including both population names of the pairwise Fst
+echo \${PREFIX}
+PREFIX=\${PREFIX%%.*}
+echo \${PREFIX}
+
+POP_UN=\${PREFIX%_*}                                                                              # retrieve both population names separately 
+echo \${POP_UN}
+POP_DEUX=\${PREFIX##*_}
+echo \${POP_DEUX}
+
+vcftools \                                                                                        # from the genotyping file, retrieve all individuals from both populations
+  --gzvcf \${INPUT} \                                                                             # and keep the 800 most differentiated positions based on the position file created at job1
+   --keep $BASE_DIR/outputs/8_hybrids/\${POP_UN}.pop \
+   --keep $BASE_DIR/outputs/8_hybrids/\${POP_DEUX}.pop \
+   --thin 5000 \
+   --out $BASE_DIR/outputs/8_hybrids/newHyb.\${POP_UN}_\${POP_DEUX} \
+   --positions $BASE_DIR/outputs/8_hybrids/\${PREFIX}_800SNPs.snps \
+   --recode                                                                                       # output with recode.vcf extension
+
+EOA
+
+
+
+# ------------------------------------------------------------------------------
+# Job 3 creates the files with 800 most differentiated SNPs
+
+jobfile3=3_format.tmp # temp file
+cat > $jobfile3 <<EOA # generate the job file
+#!/bin/bash
+#SBATCH --job-name=3_format
+#SBATCH --partition=carl.p
+#SBATCH --array=1-12
+#SBATCH --output=$BASE_DIR/logs/3_format_%A_%a.out
+#SBATCH --error=$BASE_DIR/logs/3_format_%A_%a.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=50G
+#SBATCH --time=03:00:00
+
+
+INPUT_PAIRS=$BASE_DIR/outputs/lof/8_fst.fofn                                                      # input the file of list of pairwise Fst files                                    
+PAIR=\$(cat \${INPUT_PAIRS} | head -n \${SLURM_ARRAY_TASK_ID} | tail -n 1)                        # create a job array per Fst file
+echo \${PAIR}
+
+PREFIX=\${PAIR##*/}                                                                               # retrieve pairwise comparison prefix
+echo \${PREFIX}
+PREFIX=\${PREFIX%%.*}
+echo \${PREFIX}
+
+POP_UN=\${PREFIX%_*}                                                                              # retrieve both population names of the pairwise comparison
+echo \${POP_UN}
+POP_DEUX=\${PREFIX##*_}
+echo \${POP_DEUX}
+
+grep '#' $BASE_DIR/outputs/8_hybrids/newHyb.\${POP_UN}_\${POP_DEUX}.recode.vcf > \                # retrieve all the content of the vcf file that starts with # (info at the beginning and column names)
+         $BASE_DIR/outputs/8_hybrids/newHyb.\${POP_UN}_\${POP_DEUX}.80SNPs.vcf  
+
+grep -v '#' $BASE_DIR/outputs/8_hybrids/newHyb.\${POP_UN}_\${POP_DEUX}.recode.vcf | \             # select randomly 80 SNPs out of the 800 most differentiated positions
+       shuf -n 80 | \
+       sort -k 1 -k2 >> $BASE_DIR/outputs/8_hybrids/newHyb.\${POP_UN}_\${POP_DEUX}.80SNPs.vcf     # add those 80 SNPs it to the previously created vcf files
+
+grep '#CHROM' $BASE_DIR/outputs/8_hybrids/newHyb.\${POP_UN}_\${POP_DEUX}.80SNPs.vcf | \           # from the newly 80SNPs dataset created, make a list of individuals' name
+       cut -f 10- | \                                                                             # keep the indiviuals' name in a new txt file
+       sed 's/\\t/\\n/g' > $BASE_DIR/outputs/8_hybrids/newHyb.\${POP_UN}_\${POP_DEUX}.80SNPs_individuals.txt 
+
+java -Xmx1024m -Xms512M \                                                                         # use PGDSpider2 to create a newhybrid formatted file to be able to use with NEWHYBRID
+       -jar /user/doau0129/miniconda3/share/pgdspider-2.1.1.5-0/PGDSpider2-cli.jar \
+       -inputfile $BASE_DIR/outputs/8_hybrids/newHyb.\${POP_UN}_\${POP_DEUX}.80SNPs.vcf \         # input the vcf file with 80 SNPs
+       -inputformat VCF \
+       -outputfile $BASE_DIR/outputs/8_hybrids/newHyb.\${POP_UN}_\${POP_DEUX}.80SNPs.txt \        # output the formatted file as a txt file ready to be used in NEWHYBRID
+       -outputformat NEWHYBRIDS \
+       -spid $BASE_DIR/ressources/vcf2nh.spid
+
+
+EOA
+
+
+
+# ------------------------------------------------------------------------------
+# Job 4 creates the files with 800 most differentiated SNPs
+
+jobfile4=4_nh.tmp # temp file
+cat > $jobfile4 <<EOA # generate the job file
+#!/bin/bash
+#SBATCH --job-name=4_nh
+#SBATCH --partition=carl.p
+#SBATCH --array=1-12
+#SBATCH --output=$BASE_DIR/logs/4_nh_%A_%a.out
+#SBATCH --error=$BASE_DIR/logs/4_nh_%A_%a.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=50G
+#SBATCH --time=3-03:00:00
+
+
+module load hpc-env/8.3                                                                           # load necessary environment for necessary version of R 
+module load R/4.0.2-foss-2019b
+
+INPUT_PAIRS=$BASE_DIR/outputs/lof/8_fst.fofn                                                      # input the file with list of pairwise FST files
+PAIR=\$(cat \${INPUT_PAIRS} | head -n \${SLURM_ARRAY_TASK_ID} | tail -n 1)                        # create 1 job per pairwise FST file
+echo \${PAIR}
+
+PREFIX=\${PAIR##*/}                                                                               # find the prefix corresponding to pairwise comparison in file name
+echo \${PREFIX}
+PREFIX=\${PREFIX%%.*}
+echo \${PREFIX}
+
+POP_UN=\${PREFIX%_*}                                                                              # find both the populations name of the pairwise Fst prefix
+echo \${POP_UN}
+POP_DEUX=\${PREFIX##*_}
+echo \${POP_DEUX}
+
+mkdir -p $BASE_DIR/outputs/8_hybrids/\${POP_UN}_\${POP_DEUX}_nhinput                              # create an output folder for each pairwise comparison
+scp -r $BASE_DIR/outputs/8_hybrids/newHyb.\${POP_UN}_\${POP_DEUX}.80SNPs.txt $BASE_DIR/outputs/8_hybrids/\${POP_UN}_\${POP_DEUX}_nhinput/ # copy the NEWHYBRID formatted file with 80 SNP positions to the new output folder
+scp -r $BASE_DIR/outputs/8_hybrids/newHyb.\${POP_UN}_\${POP_DEUX}.80SNPs_individuals.txt $BASE_DIR/outputs/8_hybrids/\${POP_UN}_\${POP_DEUX}_nhinput/ # copy the list of individual from both population of the pairwise comparison to the new output folder
+
+Rscript $BASE_DIR/R/newhybrids.R $BASE_DIR/outputs/8_hybrids/\${POP_UN}_\${POP_DEUX}_nhinput/     # execute the Rscript that performs NEWHYBRIDS
+
+
+EOA
+
+
+# ------------------------------------------------------------------------------
+# Job 5 make hybrid plot
+
+jobfile5=5_plots.tmp # temp file
+cat > $jobfile5 <<EOA # generate the job file
+#!/bin/bash
+#SBATCH --job-name=5_plots
+#SBATCH --partition=carl.p
+#SBATCH --output=$BASE_DIR/logs/5_plots_%A_%a.out
+#SBATCH --error=$BASE_DIR/logs/5_plots_%A_%a.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=50G
+#SBATCH --time=3-03:00:00
+
+
+module load hpc-env/8.3                                                                           # load necessary environment for necessary version of R 
+module load R/4.0.2-foss-2019b
+ml FriBidi
+ml HarfBuzz
+
+mkdir $BASE_DIR/figures/9_newhyb/                                                                 # create figure directory for NEWHYBRID plot
+
+Rscript $BASE_DIR/R/plot_hybrids.R $BASE_DIR/outputs/9_newhyb/ $BASE_DIR/figures/9_newhyb/        # run Rscript for newhybrid plot
+
+
+EOA
+
+
+
+# ********** Schedule the job launching ***********
+# -------------------------------------------------
+
+# ------------------------------------------------------------------------------
+# DATA PREPARATION
+
+if [ "$JID_RES" = "jid1" ] || [ "$JID_RES" = "jid2" ] || [ "$JID_RES" = "jid3" ] || [ "$JID_RES" = "jid4" ] || [ "$JID_RES" = "jid5" ];
+then
+  echo "*****   0_pairs         : DONE         **"
+else
+  jid0=$(sbatch ${jobfile0})
+fi
+
+
+if [ "$JID_RES" = "jid2" ] || [ "$JID_RES" = "jid3" ] || [ "$JID_RES" = "jid4" ] || [ "$JID_RES" = "jid5" ];
+then
+  echo "*****   1_snp           : DONE         **"
+elif [ "$JID_RES" = jid1 ]
+then
+  jid1=$(sbatch ${jobfile1})
+else
+  jid1=$(sbatch --dependency=afterok:${jid0##* } ${jobfile1})
+fi
+
+
+if [ "$JID_RES" = "jid3" ] || [ "$JID_RES" = "jid4" ]  || [ "$JID_RES" = "jid5" ];
+then
+  echo "*****   2_align_fastq   : DONE         **"
+elif [ "$JID_RES" = jid2 ]
+then
+  jid2=$(sbatch ${jobfile2})
+else
+  jid2=$(sbatch --dependency=afterok:${jid1##* } ${jobfile2})
+fi
+
+
+if [ "$JID_RES" = "jid4" ] || [ "$JID_RES" = "jid5" ];
+then
+  echo "*****   3_format        : DONE         **"
+elif [ "$JID_RES" = "jid3" ]
+then
+  jid3=$(sbatch ${jobfile3})
+else
+  jid3=$(sbatch --dependency=afterok:${jid2##* } ${jobfile3})
+fi
+
+
+if [ "$JID_RES" = "jid5" ];
+then
+  echo "*****   4_nh            : DONE         **"
+elif [ "$JID_RES" = "jid4" ]
+then
+  jid4=$(sbatch ${jobfile4})
+else
+  jid4=$(sbatch --dependency=afterok:${jid3##* } ${jobfile4})
+fi
+
+
+if [ "$JID_RES" = "jid5" ];
+then
+  jid5=$(sbatch ${jobfile5})
+else
+  jid5=$(sbatch --dependency=afterok:${jid4##* } ${jobfile5})
+fi
+
+
+
+# ******** Remove useless files and folders *******
+# -------------------------------------------------
+
+rm *tmp
+# rm -r $BASE_DIR/outputs/lof/
