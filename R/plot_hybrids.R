e098b91bfc048415c07944b57b5ccf15e2c92cc7
fco <fco@macflo.local>
2022-02-07 16:27:11 +0100

    hybrid pipeline

--- R/plot_hybrids.R
+++ R/plot_hybrids.R
@@ -94,8 +94,9 @@ getPofZ <- function(res_paths) {
   # Get the pairwise comparison label
   runname <- res_paths %>% 
              str_remove("newHyb.") %>% 
-             str_remove(".80SNPs.txt_Results/")
-  print(runname)
+             str_remove(".80SNPs.txt_Results/") %>%
+             sub(".*/", "", .)
+print(runname)
   
   pops <- c(str_sub(runname,1,6),str_sub(runname,-6,-1)) # Get each population of the pairwise comparison separately
   print(pops)
