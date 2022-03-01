ca207898a0fec6fb752e527c36486361c553366a
fco <fco@macflo.local>
2022-03-01 17:37:48 +0100

    modif

--- continuous_image_pca.sh
+++ continuous_image_pca.sh
@@ -15,6 +15,19 @@
 
 
 
+# Slurm parameters
+#SBATCH --job-name=continuous_images.tmp
+#SBATCH --partition=carl.p
+#SBATCH --output=$BASE_DIR/logs/continuous_images_%A_%a.out
+#SBATCH --error=$BASE_DIR/logs/continuous_images_%A_%a.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=22G
+#SBATCH --time=04:00:00
+
+
+
 # ********** Allow to enter bash options **********
 # -------------------------------------------------
 
