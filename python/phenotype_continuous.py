54dac52e5925439153625b20aa95f2c8b8ddde4b
fco <fco@macflo.local>
2022-03-10 19:28:17 +0100

    modif

--- python/phenotype_continuous.py
+++ python/phenotype_continuous.py
@@ -273,7 +273,7 @@ def plot_heatmap(b_m, rgb_m, pca, component, effect, res_path, data_name):
     rgb_im[b_m] = color_mask # <-- fill the positions of the black and white numpy image where the boolean mask is TRUE with the color values
 
     
-    fig, ax = plt.subplots(figsize=(10,8))
+    fig, ax = plt.subplots(figsize=(12,8))
     im = ax.imshow(rgb_im)
     cax = fig.add_axes([0.001, 0.01, 0.775, 0.05]) # <-- create ax for the colorbar scale
     cb = matplotlib.colorbar.ColorbarBase(cax, cmap=cmap, norm=norm, ticks = bounds, orientation='horizontal') # <-- Create a colorbar axes
@@ -285,7 +285,7 @@ def plot_heatmap(b_m, rgb_m, pca, component, effect, res_path, data_name):
     ax.set_yticklabels([]) # <-- remove y tick labels
     ax.set(xticks=[]) # <-- remove x ticks
     ax.set(yticks=[]) # <-- remove y ticks 
-    fig.subplots_adjust(top = 0.001, bottom = 0, right=.9, left = 0, 
+    fig.subplots_adjust(top = 0.9, bottom = 0, right=0, left = 0, 
             hspace = 0, wspace = 0)  # <-- Add space so the colorbar doesn't overlap the plot
     
     plt.margins(0,0)
