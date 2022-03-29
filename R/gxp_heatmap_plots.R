5cbb28376463b308cf01650b6e78e003730db9a3
fco <fco@macflo.local>
2022-03-29 16:48:22 +0200

    plots

--- R/gxp_heatmap_plots.R
+++ R/gxp_heatmap_plots.R
@@ -293,7 +293,7 @@ im["im"] <- gsub('.{4}$', '', im$image)
 # Combine PCs info with image info and calculate centroids for each species group 
 meta_table <- merge(pca_pheno, im, by = 'im') # Merge image metadata file to PCA results table 
 print(meta_table)
-centroids <- aggregate(cbind(pca_pheno[[pc_firs]],pca_pheno[[pc_second]])~spec,pca_pheno,mean) # Create centroid table for PC1 PC2 for each of the species group
+centroids <- aggregate(cbind(pca_pheno[[pc_first]],pca_pheno[[pc_second]])~spec,pca_pheno,mean) # Create centroid table for PC1 PC2 for each of the species group
 centroids %>% rename(V1 = pc_first, V2 = pc_second)
 meta_table_centroid <- merge(meta_table, centroids, by = 'spec') # Merge centroid table with the image and PCA data table
 print(meta_table_centroid)
