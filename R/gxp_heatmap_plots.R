2010e0adbeba8460fd9da47b38ff110eaa7ff449
fco <fco@macflo.local>
2022-04-04 14:00:42 +0200

    change

--- R/gxp_heatmap_plots.R
+++ R/gxp_heatmap_plots.R
@@ -334,6 +334,6 @@ p <- plot_pca(meta_table_centroid, centroids, var, f1, f2, figure_path, dataset)
 # Save the plot
 hypo_save(filename = paste0(figure_path,pc_first,"_",pc_second,"_univariate_gwas.pdf"),
           plot = p,
-          width = 100,
-          height = 100,
+          width = 15,
+          height = 12,
           limitsize = FALSE)
