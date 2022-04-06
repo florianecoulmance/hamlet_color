cb31c1ce4c36d9011394ad9b09e27a7d5b80e8ca
fco <fco@macflo.local>
2022-04-06 17:19:10 +0200

    plots

--- R/gxp_pca_univariate.R
+++ R/gxp_pca_univariate.R
@@ -226,13 +226,11 @@ pca_analysis <- function(pc_first, pc_second, pca_pheno, var, im) {
 
 # Analyses of image PCA and gwas of PC1 and PC2
 # Open all needed files
-pheno_PC <- read.csv(paste0(metadata, dataset, "_PCs.csv"), sep = ";", header= TRUE) 
-print(pheno_PC)
-
-pheno_PC <- pheno_PC %>% 
-            mutate(spec = ifelse(sample = "PL17_23nigpue", "tan", spec), 
+pheno_PC <- read.csv(paste0(metadata, dataset, "_PCs.csv"), sep = ";") %>% 
+            mutate(spec = ifelse(sample == "PL17_23nigpue", "tan", spec), 
                    sample = gsub("PL17_23nigpue", "PL17_23tanpue", sample))
 print(pheno_PC)
+
 # pca_pheno <- read.csv("/Users/fco/Desktop/PhD/1_CHAPTER1/1_GENETICS/chapter1/metadata/LAB_fullm_left_54off_59on_PCs.csv", sep = ";")
 pheno_var <- read.csv(paste0(im_path, dataset, "_var.csv"), sep = ",")
 # var <- read.csv("/Users/fco/Desktop/PhD/1_CHAPTER1/1_GENETICS/chapter1/images/continuous/LAB/LAB_fullm_left_54off_59on/LAB_fullm_left_54off_59on_var.csv", sep = ",")
