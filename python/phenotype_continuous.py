760ebafc2fb8dab9fc73802edc56e841c75524c9
fco <fco@macflo.local>
2022-04-12 16:37:05 +0200

    figure1 grren mask fullm

--- python/phenotype_continuous.py
+++ python/phenotype_continuous.py
@@ -94,7 +94,7 @@ def modify_image(im_path, bo_mask, bl_mask, effect, path_fig):
         image_blurred = image.copy()
         # print(image_blurred.shape)
 
-        image_blurred[bo_mask == 0] = 0
+        image_blurred[bo_mask == 0] = (11, 102, 35)
         # print(image_blurred.shape)
         # image_blurred[bo_mask == 1] = (11, 102, 35)
 
