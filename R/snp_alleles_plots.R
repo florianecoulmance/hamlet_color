853b89c418901fa6679b8935655b7fbea890046d
fco <fco@macflo.local>
2023-01-11 23:35:20 +0100

    pie

--- R/snp_alleles_plots.R
+++ R/snp_alleles_plots.R
@@ -104,6 +104,59 @@ pie_plots <- function(dat) {
 }
 
 
+get_leg <- function(dat) {
+
+# plot legend only 
+
+p <- ggplot(data = dat) +
+    geom_scatterpie(aes(x=x_pos, y=y_pos, group=allele, r=radius),
+                    data=dat, cols=colnames(dat)[2:14], color=NA, alpha=.8) +
+    geom_text(aes(y = y_pos + 4, x = x_pos, 
+                  label = paste0(label,"\n",allele,"\nnb individuals : ",sum)),
+              size=20) +
+    scale_fill_manual(values=c("abe"="#E5E5A1",
+                              #  "aff"="#FFB1D8",
+                               "chl"="#8B4513",
+                               "flo"="#A39CCF",
+                               "gem"="#1E90FF",
+                               "gum"="#FAA342",
+                               "gut"="#FFEA00",
+                               "ind"="#0A0082",
+                               "may"="#7EA7C2",
+                               "nig"="#1E1E1E",
+                               "pue"="#E17366",
+                               "ran"="#7EBDB3",
+                               "uni"="#CCCCCC",
+                               "tan"="#D2B48C"),
+                      labels = c("abe" = "<img src='/user/doau0129/work/chapter2/ressources/logos/H_aberrans.l.cairo.png' width='40' /><br>*H. aberrans*",
+                                #  "aff" = "<img src='/user/doau0129/work/chapter2/ressources/logos/H_tan.l.cairo.png' width='40' /><br>*H. affinis*",
+                                 "chl" = "<img src='/user/doau0129/work/chapter2/ressources/logos/H_chlorurus.l.cairo.png' width='40' /><br>*H. chlorurus*",
+                                 "flo" = "<img src='/user/doau0129/work/chapter2/ressources/logos/H_floridae.l.cairo.png' width='40' /><br>*H. floridae*",
+                                 "gem" = "<img src='/user/doau0129/work/chapter2/ressources/logos/H_gemma.l.cairo.png' width='40' /><br>*H. gemma*",
+                                 "gum" = "<img src='/user/doau0129/work/chapter2/ressources/logos/H_gumigutta.l.cairo.png' width='40' /><br>*H. gummigutta*",
+                                 "gut" = "<img src='/user/doau0129/work/chapter2/ressources/logos/H_guttavarius.l.cairo.png' width='40' /><br>*H. guttavarius*",
+                                 "ind" = "<img src='/user/doau0129/work/chapter2/ressources/logos/H_indigo.l.cairo.png' width='40' /><br>*H. indigo*",
+                                 "may" = "<img src='/user/doau0129/work/chapter2/ressources/logos/H_maya.l.cairo.png' width='40' /><br>*H. maya*",
+                                 "nig" = "<img src='/user/doau0129/work/chapter2/ressources/logos/H_nigricans.l.cairo.png' width='40' /><br>*H. nigricans*",
+                                 "pue" = "<img src='/user/doau0129/work/chapter2/ressources/logos/H_puella.l.cairo.png' width='40' /><br>*H. puella*",
+                                 "ran" = "<img src='/user/doau0129/work/chapter2/ressources/logos/H_randallorum.l.cairo.png' width='40' /><br>*H. randallorum*",
+                                 "uni" = "<img src='/user/doau0129/work/chapter2/ressources/logos/H_unicolor.l.cairo.png' width='40' /><br>*H. unicolor*",
+                                 "tan" = "<img src='/user/doau0129/work/chapter2/ressources/logos/H_sp.l.cairo.png' width='40' /><br>*Hypoplectrus* sp.")) + 
+    
+  guides(fill = guide_legend(nrow = 1)) +
+  theme(axis.title = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        legend.position = c(0.5, 0.5), # move the legend to the center
+        legend.title = element_blank(),
+        legend.text = element_text(size = 40),
+        legend.key = element_rect(fill='NA'),
+        panel.grid = element_blank(),
+        panel.border = element_blank())
+
+}
+
+
 save_plots <- function(plot, label, path_fig) {
   
   # Takes a plot and save it with label name and the figure path
@@ -158,13 +211,14 @@ for (i in names(test[,2:20])) {
   assign(i, pie_dat)
   
   p <- pie_plots(pie_dat)
-  legend <- get_legend(p) 
-  hypo_save(filename = paste0(figure_path,"legend.png"),
+  save_plots(p,i,figure_path)
+
+  l <- get_leg(pie_dat)
+  hypo_save(filename = paste0(path_fig,label,"legend.png"),
             type = "cairo",
-            plot = legend,
+            plot = l,
             width = 8.3,
             height = 2)
 
-  save_plots(p,i,figure_path)
   
 }
